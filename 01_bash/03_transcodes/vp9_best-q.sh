6f3622594b270f81b8bfa1d4c77fc6e36ba63cd5
Jorn Bergmans <jorn.bergmans@mediamonks.com>
2021-10-26 12:44:31 +0200

    Update to support alpha.

--- 01_bash/03_transcodes/vp9_best-q.sh
+++ 01_bash/03_transcodes/vp9_best-q.sh
@@ -8,5 +8,5 @@ ffmpeg -y -i "$1" -c:v libvpx-vp9 -pass 1 -b:v 250k -threads 1 -speed 4 \
 
 ffmpeg -y -i "$1" -c:v libvpx-vp9 -pass 2 -b:v 2500k -threads 1 -speed 0 \
   -tile-columns 0 -frame-parallel 0 -auto-alt-ref 1 -lag-in-frames 25 \
-  -pix_fmt yuv420p -g 100 -aq-mode 0 -c:a opus -strict -2 -b:a 128k \
-  -f webm $1-vp9.webm
+  -pix_fmt yuva420p -g 100 -aq-mode 0 -c:a opus -strict -2 -b:a 128k \
+  -f webm ${1%.*}-vp9.webm
